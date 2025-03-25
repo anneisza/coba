e2f4e0e3c65321a9581a0a7f4d9f6eb62553186f
Riza Fitria Istiqomah Zaitunah <rizaanneisza@gmail.com>
2025-03-25 11:09:48 +0700

    coba 3

--- coba.cpp
+++ coba.cpp
@@ -3,3 +3,32 @@ using namespace std;
 
 int arr[20]; // Membuat Array dengan panjang data 20
 int n; // Membuat Variale inputan n
+
+void input()
+{ // Procedure Input
+    while (true)
+    {
+        cout << "Masukkan Jumlah Data pada Array : "; // Membuat Inputan jumlah element Array
+        cin >> n; // memanggil variable inputan n
+
+        if (n <= 20)
+        { // Membuat Kondisi n tidak lebih dari 20
+            break;
+        }
+            else
+        {
+            cout << "\nArray yang anda masukkan maksimal 20 Elemen.\n"; // Menampilkan Pesan jika data lebih dari 20
+        }
+    }
+    cout << endl; // Membuat jarak per baris program
+    cout << "=================" << endl; // Membuat tampilan susunan data element array
+    cout << "Masukkan Element Array" << endl;
+    cout << "=================" << endl;
+
+    for (int i = 0; i < n; i++) //Menggunakan perulangan for untuk menyimpan data pada array
+    {
+        cout << "Data ke-" << (i + 1) << ": "; //Memasukkan atau menginputkan nilai data n
+        cin >> arr[i]; // Menyimpan nilai data n ke dalam array arr
+    }
+}
+
